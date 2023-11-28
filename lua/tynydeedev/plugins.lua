0f99baced6b79d6c6d7ecf6dca4dd042e7de64ff
Dat Vu <tynydeedev@gmail.com>
2023-11-28 21:27:28 +0700

    update nvim completion display: add border

--- lua/tynydeedev/plugins.lua
+++ lua/tynydeedev/plugins.lua
@@ -46,7 +46,21 @@ local plugins = {
 	{ "williamboman/mason-lspconfig.nvim" },
 	{ "neovim/nvim-lspconfig" },
 	{ "hrsh7th/cmp-nvim-lsp" },
-	{ "hrsh7th/nvim-cmp" },
+	{
+		"hrsh7th/nvim-cmp",
+		opts = {
+			window = {
+				completion = {
+					border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+					winhighlight = "Normal:CmpPmenu,FloatBorder:CmpBorder,CursorLine:PmenuSel,Search:None",
+				},
+				documentation = {
+					border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+					winhighlight = "Normal:CmpPmenu,FloatBorder:CmpBorder,CursorLine:PmenuSel,Search:None",
+				},
+			},
+		},
+	},
 	{ "L3MON4D3/LuaSnip" },
 	{
 		"windwp/nvim-autopairs",
