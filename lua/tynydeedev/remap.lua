0f3a835502d5be41945619a9a8ccb64e3faf8aaf
Dat Vu <tynydeedev@gmail.com>
2023-12-28 00:16:05 +0700

    update keymap

--- lua/tynydeedev/remap.lua
+++ lua/tynydeedev/remap.lua
@@ -1,5 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
 
 -- Nvim-Tree
 vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<cr>")
