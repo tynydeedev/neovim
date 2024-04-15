f281c1e6e773c7d1a163257d367179c8e85cd4e5
Dat Vu <tynydeedev@gmail.com>
2024-04-16 00:15:53 +0700

    feat: 🎸 update vue lsp

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -63,6 +63,23 @@ require("mason-lspconfig").setup({
 				},
 			})
 		end,
+		["tsserver"] = function()
+			local mason_registry = require("mason-registry")
+			local vue_language_server_path = mason_registry.get_package("vue-language-server"):get_install_path()
+				.. "/node_modules/@vue/language-server"
+			require("lspconfig").tsserver.setup({
+				init_options = {
+					plugins = {
+						{
+							name = "@vue/typescript-plugin",
+							location = vue_language_server_path,
+							languages = { "vue" },
+						},
+					},
+				},
+				filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+			})
+		end,
 	},
 })
 
